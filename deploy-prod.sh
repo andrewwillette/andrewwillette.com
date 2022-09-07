7190333ce71b3a9b915de2b0fc629404e58ddb70
andrewwillette <willette.andrew1846@gmail.com>
2022-09-06 23:33:16 -0500

    master: docker not deploying to prod, backend executable cannot be ran in ubuntu-hosted docker container. weird.

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -3,9 +3,6 @@ export GIT_COMMIT=$(git rev-parse HEAD)
 cd backend
 env GOOS=linux GOARCH=arm64 go build .
 cd ..
-cd frontend
-npm install && npm run build
-cd ..
 docker context use andrewwilletteWebsiteAws
 docker-compose build
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
