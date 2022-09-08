a6bc0b20210edf3a1ec62c179c2ac3158fff01c8
andrewwillette <willette.andrew1846@gmail.com>
2022-09-07 19:28:32 -0500

    master: docker now deploying backend successfully

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 export GIT_COMMIT=$(git rev-parse HEAD)
-cd backend
-env GOOS=linux GOARCH=arm64 go build .
-cd ..
 docker context use andrewwilletteWebsiteAws
 docker-compose build
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
