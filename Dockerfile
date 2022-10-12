6c024f3b22b7b4bfe4de503bf52a71d6a7f90264
andrewwillette <willette.andrew1846@gmail.com>
2022-10-11 23:21:35 -0500

    master: updating docker config

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,6 @@ ARG GIT_COMMIT_ARG=unspecified
 
 ENV GIT_COMMIT=$GIT_COMMIT
 WORKDIR /awillettebackend
-RUN mkdir /awillettebackend/db
 COPY . .
 ENV CGO_ENABLED=1
 RUN go build .
--- deploy-prod.sh
+++ deploy-prod.sh
@@ -2,4 +2,4 @@
 export GIT_COMMIT=$(git rev-parse HEAD)
 docker context use andrewwilletteWebsiteAws
 docker-compose build
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+docker-compose -f docker-compose.yml up -d
