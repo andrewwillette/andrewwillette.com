1eeedcd4581de8324243c26136448af8e1b55227
andrewwillette <willette.andrew1846@gmail.com>
2022-11-05 12:02:42 -0500

    master: removing health check, moving favicon to s3

--- server/server.go
+++ server/server.go
@@ -1,42 +1,35 @@
 package server
 
 import (
-	"encoding/json"
 	"fmt"
 	"html/template"
 	"io"
 	"log"
 	"net/http"
-	"os"
 	"path/filepath"
 	"runtime"
-	"runtime/debug"
 
 	"github.com/andrewwillette/keyOfDay/key"
-	"github.com/andrewwillette/willette_api/logging"
 	"github.com/labstack/echo/v4"
-	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
 const (
-	getSoundcloudAllEndpoint     = "/get-soundcloud-urls"
-	addSoundcloudEndpoint        = "/add-soundcloud-url"
-	deleteSoundcloudEndpoint     = "/delete-soundcloud-url"
-	keyOfDayEndpoint             = "/keyOfDay"
-	healthEndpoint               = "/health"
-	loginEndpoint                = "/login"
+	homeEndpoint                 = "/"
+	musicEndpoint                = "/music"
+	resumeEndpoint               = "/resume"
+	cssEndpoint                  = "/static/main.css"
+	keyOfDayEndpoint             = "/kod"
 	updateSoundcloudUrlsEndpoint = "/update-soundcloud-urls"
 	port                         = 80
 )
 
 func StartServer() {
 	e := echo.New()
-	e.GET("/", handleHomePage)
-	e.GET("/resume", handleResumePage)
-	e.GET("/music", handleMusicPage)
-	e.GET("/kod", handleKeyOfDay)
+	e.GET(homeEndpoint, handleHomePage)
+	e.GET(resumeEndpoint, handleResumePage)
+	e.GET(musicEndpoint, handleMusicPage)
+	e.GET(keyOfDayEndpoint, handleKeyOfDay)
 	e.File("/static/main.css", "static/main.css")
-	e.File("/static/illuminati.gif", "static/illuminati.gif")
 	e.Renderer = getTemplateRenderer()
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", port)))
 }
@@ -94,66 +87,4 @@ func getTemplateRenderer() *Template {
 		templates: template.Must(template.ParseGlob(fmt.Sprintf("%s/templates/*.tmpl", basepath))),
 	}
 	return t
-}
-
-type keyOfDayService interface {
-	GetKeyOfDay() string
-}
-
-func getNewRelicApp() *newrelic.Application {
-	newrelicLicense := os.Getenv("NEW_RELIC_LICENSE")
-	app, err := newrelic.NewApplication(
-		newrelic.ConfigAppName("go-andrewwillette"),
-		newrelic.ConfigLicense(newrelicLicense),
-	)
-	if err != nil {
-		logging.GlobalLogger.Error().Msgf("Failed to start new relic app, newrelic key: %s", newrelicLicense)
-	}
-	return app
-}
-
-// newServer setup server with endpoints
-var buildTime = func() string {
-	if info, ok := debug.ReadBuildInfo(); ok {
-		for _, setting := range info.Settings {
-			if setting.Key == "vcs.time" {
-				return setting.Value
-			}
-		}
-	}
-	return ""
-}()
-
-var commit = func() string {
-	cm := os.Getenv("GIT_COMMIT")
-	if cm != "" {
-		return cm
-	} else {
-		return "GIT_COMMIT not set"
-	}
-	// if info, ok := debug.ReadBuildInfo(); ok {
-	// 	for _, setting := range info.Settings {
-	// 		if setting.Key == "vcs.revision" {
-	// 			return setting.Value
-	// 		}
-	// 	}
-	// }
-	// return ""
-}()
-
-type healthCheckResp struct {
-	Buildtime string `json:"buildTime"`
-	Commit    string `json:"gitCommit"`
-}
-
-func getKeyOfDay(c echo.Context) error {
-	logging.GlobalLogger.Info().Msg("Calling key of day.")
-	c.Response().Header().Set("Content-Type", "application-json")
-	c.Response().Header().Set("Access-Control-Allow-Origin", "*")
-	if err := json.NewEncoder(c.Response()).Encode(key.GetKeyOfDay()); err != nil {
-		const errMsg = "Failed to encode keyOfDay string"
-		logging.GlobalLogger.Err(err).Msg(errMsg)
-		return c.String(http.StatusInternalServerError, errMsg)
-	}
-	return c.String(http.StatusOK, "")
 }
