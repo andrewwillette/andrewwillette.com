9e2243c465abfbc645cf846d5eb0320a59a6b179
andrewwillette <willette.andrew1846@gmail.com>
2023-07-29 09:18:24 -0500

    master: defining soundcloud urls in server-side structs

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/andrewwillette/willette_api
 
-go 1.19
+go 1.20
 
 require (
 	github.com/andrewwillette/keyofday v0.0.0-20221211003701-cfeaf1ddb191
@@ -12,7 +12,6 @@ require (
 )
 
 require (
-	github.com/BurntSushi/toml v1.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
@@ -25,6 +24,5 @@ require (
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -77,7 +77,7 @@ func handleResumePage(c echo.Context) error {
 
 // handleMusicPage handles returning the music template
 func handleMusicPage(c echo.Context) error {
-	err := c.Render(http.StatusOK, "musicpage", nil)
+	err := c.Render(http.StatusOK, "musicpage", musicData)
 	if err != nil {
 		log.Println(err)
 		return err
@@ -105,9 +105,73 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
+type Song struct {
+	Title string
+	URL   string
+}
+type MusicPageData struct {
+	Songs []Song
+}
+
 var (
 	_, b, _, _ = runtime.Caller(0)
 	basepath   = filepath.Dir(b)
+	musicData  = MusicPageData{
+		Songs: []Song{
+			{
+				Title: "Swallowtail Jig",
+				URL:   "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1511471356&color=%23b0a472&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true",
+			},
+			{
+				Title: "Sherry",
+				URL:   "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1412600335&color=%233799bb&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true",
+			},
+			{
+				Title: "Raggedy Ann",
+				URL:   "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1353192850&color=%23e25862&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true",
+			},
+			{
+				Title: "Leather Britches",
+				URL:   "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1340417752&color=%237c643d&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true",
+			},
+			{
+				Title: "Carrol County Blues",
+				URL:   "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1299581302&color=%23e25862&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true",
+			},
+			{
+				Title: "Flop Eared Mule",
+				URL:   "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1279484290&color=%2356bb37&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true",
+			},
+			{
+				Title: "Dixie",
+				URL:   "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1279435306&color=%23e193db&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true",
+			},
+			{
+				Title: "Polecat Blues",
+				URL:   "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1244352139&color=%23f3143a&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true",
+			},
+			{
+				Title: "Wednesday Night Waltz",
+				URL:   "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1241630236&color=%23e6d5d5&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true",
+			},
+			{
+				Title: "Down The Road Somewhere",
+				URL:   "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1206283588&color=%235a1c1c&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true",
+			},
+			{
+				Title: "Benton's Dream",
+				URL:   "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1202201206&color=%23ccd7c8&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true",
+			},
+			{
+				Title: "Sugar In The Gourd",
+				URL:   "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1177441174&color=%23c224c3&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true",
+			},
+			{
+				Title: "Walking In My Sleep",
+				URL:   "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1144703368&color=%23b0a472&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true",
+			},
+		},
+	}
 )
 
 // getTemplateRenderer returns a template renderer for my echo webserver
