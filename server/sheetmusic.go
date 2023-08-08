7c2e75a969b165b259cda95adecf0804b3e33fb8
andrewwillette <willette.andrew1846@gmail.com>
2023-08-07 23:58:04 -0500

    master: moving music and sheet music values to separate files

---
+++ server/sheetmusic.go
@@ -0,0 +1,16 @@
+package server
+
+var (
+	sheetmusicData = SheetMusicPageData{
+		Sheets: []DropboxReference{
+			{
+				Name: "Cumberland Gap",
+				URL:  "https://www.dropbox.com/scl/fi/9vnjhsojyefsutz4yzt00/Cumberland-Gap.pdf?rlkey=i8ueptsmvhfmi59ww7h3q9dij&dl=0",
+			},
+			{
+				Name: "Benton's Dream",
+				URL:  "https://www.dropbox.com/scl/fi/i4c0x7z8i8eis0gvyxqrl/Benton-s-Dream.pdf?dl=0&rlkey=ra3i5gf5kyu6ulup5uqezpzr9",
+			},
+		},
+	}
+)
