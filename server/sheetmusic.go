72040dab9a759a299536ffe24e5386f69a9e956c
andrewwillette <willette.andrew1846@gmail.com>
2023-08-22 21:53:46 -0500

    master: adding sheet music

--- server/sheetmusic.go
+++ server/sheetmusic.go
@@ -8,8 +8,56 @@ var (
 				URL:  "https://www.dropbox.com/scl/fi/9vnjhsojyefsutz4yzt00/Cumberland-Gap.pdf?rlkey=i8ueptsmvhfmi59ww7h3q9dij&dl=0",
 			},
 			{
+				Name: "Texas Crapshooter",
+				URL:  "https://www.dropbox.com/scl/fi/3a6rtp1nr9gsp7v1o96ue/Texas-Crapshooter.pdf?rlkey=e7ofjv00sirynyj30j5lkdalg&dl=0",
+			},
+			{
 				Name: "Benton's Dream",
 				URL:  "https://www.dropbox.com/scl/fi/i4c0x7z8i8eis0gvyxqrl/Benton-s-Dream.pdf?dl=0&rlkey=ra3i5gf5kyu6ulup5uqezpzr9",
+			},
+			{
+				Name: "Polecat Blues",
+				URL:  "https://www.dropbox.com/scl/fi/vbujvvahgnm2x42r8ynls/Polecat-Blues.pdf?rlkey=4kdf38nmotl55mkdbk915xm86&dl=0",
+			},
+			{
+				Name: "Sherry",
+				URL:  "https://www.dropbox.com/scl/fi/6cw6uzemja7knvau99n9r/Sherry.pdf?rlkey=u0qpuglnqj0j499e4uojxo0sb&dl=0",
+			},
+			{
+				Name: "Leather Britches",
+				URL:  "https://www.dropbox.com/scl/fi/pn4pnf797x0iqaonr1a8h/Leather-Britches.pdf?rlkey=lvzzvsank7s58vhhk9mpuw2gh&dl=0",
+			},
+			{
+				Name: "Carrol County Blues",
+				URL:  "https://www.dropbox.com/scl/fi/67b37hd50xn69nqr37o32/Carrol-county-blues.pdf?rlkey=tmrcqs2qvpaaatgrtvc74yeor&dl=0",
+			},
+			{
+				Name: "Big Mon",
+				URL:  "https://www.dropbox.com/scl/fi/o4mhjga3bmavayp5bpxs4/Big-Mon.pdf?rlkey=t5fg0750oi767qljnad6z5bob&dl=0",
+			},
+			{
+				Name: "Flop Eared Mule",
+				URL:  "https://www.dropbox.com/scl/fi/6gftl0pl4qwmal7nakjt6/Flop-Eared-Mule.pdf?rlkey=aeozb6gwvgdlg1813c8shzqc4&dl=0",
+			},
+			{
+				Name: "Down South In Dixie",
+				URL:  "https://www.dropbox.com/scl/fi/5dvldnguxxmn3vy5n8yzm/Down-South-In-Dixie.pdf?rlkey=1nvtabqzzbvf3pm1th6rrafvz&dl=0",
+			},
+			{
+				Name: "Wednesday Night Waltz",
+				URL:  "https://www.dropbox.com/scl/fi/vri8tdzgjddy6bzugq2ma/Wednesday-Night-Waltz.pdf?rlkey=jx2hjze0nqfa2pmvze7w8pjpi&dl=0",
+			},
+			{
+				Name: "Down The Road",
+				URL:  "https://www.dropbox.com/scl/fi/ioz5a40t97cohit78uvgi/Theres-A-Brownskin-Girl-Down-The-Road-Somewhere.pdf?rlkey=8nzc2p6a25oi4qky4hm5n8gln&dl=0",
+			},
+			{
+				Name: "Sugar In The Gourd",
+				URL:  "https://www.dropbox.com/scl/fi/pixivlknf2lq9rm1cq3gs/Sugar-in-the-Gourd.pdf?rlkey=a049bgr8vghi7qwr5cck3udd7&dl=0",
+			},
+			{
+				Name: "Walkin' In My Sleep",
+				URL:  "https://www.dropbox.com/scl/fi/jf0spl77gw15hk5c99t4r/Walking-In-My-Sleep.pdf?rlkey=jfx1jp44l3c2cr8vqwohcffw3&dl=0",
 			},
 		},
 	}
