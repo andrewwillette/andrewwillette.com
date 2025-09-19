905f42ddbc2c98edb0fbd7296cb878af46c693e7
andrewwillette <willette.andrew1846@gmail.com>
2025-09-18 21:55:02 -0500

    removing docker configurations

--- deploy-prod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker context use andrewwilletteWebsiteAws
-docker compose -f docker-compose-prod.yml down
-docker compose -f docker-compose-prod.yml build
-docker compose -f docker-compose-prod.yml up -d
