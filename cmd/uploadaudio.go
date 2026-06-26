76e90d2abcbc70949e5f30595d20387a69642324
andrewwillette <willette.andrew1846@gmail.com>
2026-06-25 21:35:05 -0500

    adding destination directory support for upload audio

--- cmd/uploadaudio.go
+++ cmd/uploadaudio.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/andrewwillette/andrewwillettedotcom/aws"
 	"github.com/andrewwillette/andrewwillettedotcom/images"
@@ -16,6 +17,7 @@ import (
 
 var audioFilePath string
 var audioFileDir string
+var audioResultDir string
 
 var uploadAudioCmd = &cobra.Command{
 	Use:   "upload-audio",
@@ -40,6 +42,7 @@ var uploadAudioCmd = &cobra.Command{
 func init() {
 	uploadAudioCmd.Flags().StringVarP(&audioFilePath, "file", "f", "", "Path to audio file")
 	uploadAudioCmd.Flags().StringVarP(&audioFileDir, "dir", "d", "", "Directory that contains audio file")
+	uploadAudioCmd.Flags().StringVarP(&audioResultDir, "result-dir", "r", "", "Directory to move the uploaded file into (with timestamp suffix)")
 	rootCmd.AddCommand(uploadAudioCmd)
 }
 
@@ -70,6 +73,23 @@ func uploadAudioFileFromDir(dir string) error {
 	return uploadAudioWithImage(fileMap[selected])
 }
 
+// moveToResultDir moves src into destDir, inserting a timestamp before the extension.
+// e.g. song.mp3 → destDir/song_20260625_153045.mp3
+func moveToResultDir(src, destDir string) error {
+	if info, err := os.Stat(destDir); err != nil || !info.IsDir() {
+		return fmt.Errorf("result-dir %q is not a valid directory", destDir)
+	}
+	ext := filepath.Ext(src)
+	base := strings.TrimSuffix(filepath.Base(src), ext)
+	ts := time.Now().Format("20060102_150405")
+	dest := filepath.Join(destDir, fmt.Sprintf("%s_%s%s", base, ts, ext))
+	if err := os.Rename(src, dest); err != nil {
+		return fmt.Errorf("failed to move %s to %s: %w", src, dest, err)
+	}
+	log.Info().Msgf("Moved %s → %s", src, dest)
+	return nil
+}
+
 func uploadAudioToS3(audioFile string) error {
 	log.Info().Msgf("Uploading audio file %s to S3...", audioFile)
 	if !isValidAudioFile(audioFile) {
@@ -79,30 +99,33 @@ func uploadAudioToS3(audioFile string) error {
 }
 
 func uploadAudioWithImage(audioFile string) error {
-	// Upload audio
 	if err := uploadAudioToS3(audioFile); err != nil {
 		return err
 	}
 
-	// Generate cover art image
 	log.Info().Msg("Generating cover art image...")
 	imagePath, err := images.GenerateSingleCoverArt(audioFile)
 	if err != nil {
 		return fmt.Errorf("failed to generate cover art: %w", err)
 	}
 
-	// Upload image
 	log.Info().Msgf("Uploading cover art image %s to S3...", imagePath)
 	if err := aws.UploadAudioImageToS3(imagePath); err != nil {
 		return fmt.Errorf("failed to upload cover art: %w", err)
 	}
 
-	// Clean up generated image
 	if err := os.Remove(imagePath); err != nil {
 		log.Warn().Msgf("Could not delete generated image: %v", err)
 	}
 
 	log.Info().Msg("Audio and cover art uploaded successfully!")
+
+	if audioResultDir != "" {
+		if err := moveToResultDir(audioFile, audioResultDir); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
